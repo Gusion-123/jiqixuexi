17872cc5646e6be898a505175d6e227b5514bad7
Gusion-123 <2151944873@qq.com>
2024-06-19 22:38:56 +0800

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,85 @@
+import os
+import time
+
+class Model(object):
+
+    def __init__(self):
+        self.path = os.getcwd()
+        train_file = 'data/weibo_train_data.txt'
+        predict_data_file = 'data/weibo_predict_data.txt'
+        self.result_file = 'data/weibo_result_data.txt'
+        self.train_data_path = os.path.join(self.path, train_file)
+        self.predict_data_path = os.path.join(self.path, predict_data_file)
+
+    def fit(self, train_list):
+        max_precision = 0
+        best_f = 0; best_c = 0; best_l = 0
+        for line in set(train_list):
+            numerator = 0   # 分子
+            denominator = 0 # 分母
+            line_dict = line.split(',')
+            i = int(line_dict[0])
+            j = int(line_dict[1])
+            k = int(line_dict[2])
+            for record in train_list:
+                record_dict = record.split(',')
+                fr = int(record_dict[0])
+                cr = int(record_dict[1])
+                lr = int(record_dict[2])
+                df = float(abs(i - fr) / (fr + 5.0))
+                dc = float(abs(j - cr) / (cr + 3.0))
+                dl = float(abs(k - lr) / (lr + 3.0))
+                precision_i = 1 - 0.5 * df - 0.25 * dc - 0.25 * dl
+                count_i = fr + cr + lr
+                count_i = 100 if count_i > 100 else count_i
+                sgn_i = 0.8 if precision_i > 0.8 else 0.0
+                denominator += count_i + 1
+                numerator += (count_i + 1) * sgn_i
+            if denominator != 0:  # 避免除以零错误
+                precision = numerator / denominator
+                if precision > max_precision:
+                    max_precision = precision
+                    best_f = i
+                    best_c = j
+                    best_l = k
+        return "%s,%s,%s" % (best_f, best_c, best_l)
+
+    def get_history_fcl(self):
+        map = {}
+        with open(self.train_data_path, encoding='utf-8') as f:
+            for line in f.readlines():
+                line_dict = line.strip().split('\t')
+                if len(line_dict) >= 6:  # 确保行分割正确
+                    uid = line_dict[0]
+                    forcast_comment_like = "%s,%s,%s" % (line_dict[3], line_dict[4], line_dict[5])
+                    if uid not in map:
+                        map[uid] = []
+                    map[uid].append(forcast_comment_like)
+        return map
+
+    def train_model(self):
+        model = {}
+        map = self.get_history_fcl()
+        for uid in map:
+            model[uid] = self.fit(map[uid])
+        return model
+
+    def output_result(self):
+        model = self.train_model()
+        result = []
+        with open(self.predict_data_path, encoding='utf-8') as f:
+            for line in f.readlines():
+                line_dict = line.strip().split('\t')
+                if len(line_dict) >= 2:  # 确保行分割正确
+                    uid = line_dict[0]
+                    mid = line_dict[1]
+                    predict = model.get(uid, "0,0,0")
+                    result.append("%s\t%s\t%s\n" % (uid, mid, predict))
+        with open(self.result_file, 'w+', encoding='utf-8') as f:
+            f.writelines(result)
+
+if __name__ == '__main__':
+    start_time = time.time()
+    Model().output_result()
+    end_time = time.time()
+    print("total cost time: %s seconds" % int(end_time - start_time))
